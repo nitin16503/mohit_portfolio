74ee66c51a06a7c2f64517b27621fec5a9a9447d
mohitsharma012 <stayer.mohit@gmail.com>
2024-02-18 00:40:06 +0530

    add files 90

--- mohitji/settings.py
+++ mohitji/settings.py
@@ -117,11 +117,17 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
-STATIC_URL = 'static/'
-STATICFILES_DIRS = [
-    BASE_DIR / 'static',
-]
+
+import os
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+# Other settings...
+
+STATIC_URL = '/static/'
+
+# Define the directory where Django will collect static files for deployment
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
